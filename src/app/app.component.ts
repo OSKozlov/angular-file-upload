6ad5afb68271ed02504df603fa72fa4c59fb644d
Oleksandr Kozlov <Oleksandr.Kozlov@halliburton.com>
2018-09-03 19:21:59 +0300

    angular file upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'File upload';
+  description = 'Choose file';
+}
